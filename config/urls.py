13e0faf3412d2dbbf4d12c74d34ab33dd8a9a32b
canalescanorff6-source <canalescanorff6@gmail.com>
2026-05-19 23:38:49 -0300

    Delete config directory

--- config/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('', lambda request: redirect('core:dashboard' if request.session.get('email') else 'core:login'), name='home'),
-    path('', include('core.urls')),
-]
